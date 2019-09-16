ddadb9903603dfdb094a179f41eface9e2c88438
renton18 <gakituka18@gmail.com>
2019-09-16 23:43:06 +0900

    コミット

--- B-EV4D-GH17-R/B_EV4D_GH17_R.cs
+++ B-EV4D-GH17-R/B_EV4D_GH17_R.cs
@@ -41,7 +41,6 @@ namespace B_EV4D_GH17_R
 
         public static void Print()
         {
-
             switch (font)
             {
                 case BARCODE_FONT.CODE39:
@@ -68,33 +67,66 @@ namespace B_EV4D_GH17_R
             IntPtr hdc = e.Graphics.GetHdc();
 
             //バーコードの印字
-            int xPos1 = 100;
-            int yPos1 = 150;
             //バーコードの場合は9番目の引数を0にする必要あり
-            IntPtr mFont1 = CreateFont(300, 0, 0, 0, 0, false, false, false, 0, 0, 0, 0, 0, barcodeFont);
-            SelectObject(hdc, mFont1);
-            TextOut(hdc, xPos1, yPos1, datas[0], datas[0].Length);
-            DeleteObject(mFont1);
+            IntPtr mFont = CreateFont(300, 0, 0, 0, 0, false, false, false, 0, 0, 0, 0, 0, barcodeFont);
+            SelectObject(hdc, mFont);
+            TextOut(hdc, 100, 150, datas[0], datas[0].Length);
+            DeleteObject(mFont);
 
             //文字列のフォント
             String stringFontName = "Meiryo UI";
 
             //文字列1
-            int xPos2 = 200;
-            int yPos2 = 360;
-            //文字列の場合は9番目の引数を128にする必要あり
-            IntPtr mFont2 = CreateFont(50, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            IntPtr mFont1 = CreateFont(100, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont1);
+            String printString1 = "文字列１";
+            TextOut(hdc, 5, 5, printString1, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString1));
+            DeleteObject(mFont1);
+            //文字列2
+            IntPtr mFont2 = CreateFont(100, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
             SelectObject(hdc, mFont2);
-            TextOut(hdc, xPos2, yPos2, datas[1], Encoding.GetEncoding("Shift_JIS").GetByteCount(datas[1]));
+            String printString2 = "文字列２";
+            TextOut(hdc, 300, 5, printString2, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString2));
             DeleteObject(mFont2);
 
-            //文字列2
-            int xPos3 = 10;
-            int yPos3 = 10;
-            IntPtr mFont3 = CreateFont(100, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            //文字列3
+            IntPtr mFont3 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
             SelectObject(hdc, mFont3);
-            TextOut(hdc, xPos3, yPos3, datas[2], Encoding.GetEncoding("Shift_JIS").GetByteCount(datas[2]));
+            String printString3 = "文字列３";
+            TextOut(hdc, 5, 105, printString3, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString3));
             DeleteObject(mFont3);
+            //文字列4
+            IntPtr mFont4 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont4);
+            String printString4 = "文字列４";
+            TextOut(hdc, 300, 105, printString4, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString4));
+            DeleteObject(mFont4);
+
+            //文字列5
+            IntPtr mFont5 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont5);
+            String printString5 = "文字列5";
+            TextOut(hdc, 5, 165, printString5, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString5));
+            DeleteObject(mFont5);
+            //文字列6
+            IntPtr mFont6 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont6);
+            String printString6 = "文字列6";
+            TextOut(hdc, 300, 165, printString6, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString6));
+            DeleteObject(mFont6);
+
+            //文字列7
+            IntPtr mFont7 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont7);
+            String printString7 = "文字列7";
+            TextOut(hdc, 5, 225, printString7, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString7));
+            DeleteObject(mFont7);
+            //文字列8
+            IntPtr mFont8 = CreateFont(60, 0, 0, 0, 0, false, false, false, 128, 0, 0, 0, 0, stringFontName);
+            SelectObject(hdc, mFont8);
+            String printString8 = "文字列8";
+            TextOut(hdc, 300, 225, printString8, Encoding.GetEncoding("Shift_JIS").GetByteCount(printString8));
+            DeleteObject(mFont8);
 
             //デバイスコンテキストの開放　これは必ず必要
             e.Graphics.ReleaseHdc(hdc);
